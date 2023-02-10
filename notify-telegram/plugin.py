31b0feb3c4ef8a54789478986bc2aa0a8b2153b0
Nick <nickdewitt115@gmail.com>
2023-02-10 15:18:51 -0500

    Updated plugin code so user settings menu appears hopefully

--- notify-telegram/plugin.py
+++ notify-telegram/plugin.py
@@ -1,33 +1,21 @@
-import os
 import requests
 from unmanic.libs.unplugins.settings import PluginSettings
 from unmanic.libs.system import System
 
 
 class Settings(PluginSettings):
-    """
-    An object to hold a dictionary of settings accessible to the Plugin
-    class and able to be configured by users from within the Unmanic WebUI.
-    This class has a number of methods available to it for accessing these settings:
-        > get_setting(<key>)            - Fetch a single setting value. Or leave the 
-                                        key argument empty and return the full dictionary.
-        > set_setting(<key>, <value>)   - Set a singe setting value.
-                                        Used by the Unmanic WebUI to save user settings.
-                                        Settings are stored on disk in order to be persistent.
-    """
     settings = {
-        "Execute Command": True,
-        "userToken": "custom-string",
-        "userChatID": "custom-string"
+        #"Execute Command": True,
+        'userToken': '',
+        "userChatID": '',
     }
     
 
 
 
 
-def notify(source_data):
-    Token = PluginSettings.get_setting(userToken)
-    Chat_ID = PluginSettings.get_setting(userChatID)
+def notify(source_data, Token, Chat_ID):
+
     #if data.get('task_processing_success') and data.get('file_move_processes_success'):
         #message = 
     message = "hello from your telegram bot"
@@ -47,7 +35,13 @@ def on_postprocessor_task_results(data):
     :param data:
     :return:
     """
-
-    notify(data.get('source_data'))
+        # Configure settings object (maintain compatibility with v1 plugins)
+    if data.get('library_id'):
+        settings = Settings(library_id=data.get('library_id'))
+    else:
+        settings = Settings()
+    Token = settings.get_setting('userToken')
+    Chat_ID = settings.get_setting('userChatID')
+    notify(data.get('source_data'), Token, Chat_ID)
 
     return data
